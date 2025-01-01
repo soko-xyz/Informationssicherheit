2caffe781b7c50a2dced1b430440f7e5d12aa32c
Joshua Sokolowski <joshua.sokolowski@outlook.de>
2025-01-01 09:52:45 +0100

    init

---
+++ src/main/kotlin/cc/sokolowski/informationssicherheit/InformationssicherheitApplication.kt
@@ -0,0 +1,11 @@
+package cc.sokolowski.informationssicherheit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InformationssicherheitApplication
+
+fun main(args: Array<String>) {
+    runApplication<InformationssicherheitApplication>(*args)
+}
---
+++ src/test/kotlin/cc/sokolowski/informationssicherheit/InformationssicherheitApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.sokolowski.informationssicherheit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InformationssicherheitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
